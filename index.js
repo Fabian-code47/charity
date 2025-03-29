92fee135aa7bfda8ff8923da119c1a2c86197640
Fabian Ciobanu <fabi@192.168.1.128>
2025-03-29 19:22:51 +0200

    login

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("test");
+const passwordEyeDiv = document.querySelector(".password-eye");
+
+const passwordEyeInput = document.querySelector(".password-input");
+const passwordEyeImg = document.querySelector(".password-eye-img");
+
+const togglePasswordEye = () => {
+  if (passwordEyeInput.type == "text") {
+    passwordEyeInput.type = "password";
+    passwordEyeImg.src = "./images/show.png";
+  } else {
+    passwordEyeInput.type = "text";
+    passwordEyeImg.src = "./images/hide.png";
+  }
+};
+
+passwordEyeDiv.addEventListener("click", togglePasswordEye);
